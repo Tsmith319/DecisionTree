ec7b1a5147ea4557320d22b39010171399fd0e74
Taylor Smith <TaySmith16@gmail.com>
2019-04-23 15:21:49 -0600

    Create run_script.sh

---
+++ logisticRegression/run_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo "Running Stochastic Gradient Descent for MAP"
+echo " "
+python MAP.py
+echo " "
+echo "Running Stochastic Gradient Descent for MLE"
+echo " "
+python ML.py
+echo " "
+echo "Done with script"
